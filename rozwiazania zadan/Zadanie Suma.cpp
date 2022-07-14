edc11335c1eaf4dfa6961c0382169b104af2d875
Kalina <102462480+AlgorytmicznaPrzygodaKaliny@users.noreply.github.com>
2022-07-14 13:49:57 +0200

    Update Zadanie Suma.cpp

--- rozwiazania zadan/Zadanie Suma.cpp
+++ rozwiazania zadan/Zadanie Suma.cpp
@@ -9,9 +9,8 @@ using namespace std;
 int main() {
 	int k;
 	cin >> k;	
-    
-    for(int i=0; i<k; i++) cout << 1;
-    for(int i=0; i<k-1; i++) cout << 0;
+	for(int i=0; i<k; i++) cout << 1;
+	for(int i=0; i<k-1; i++) cout << 0;
     
 	return 0;
 }
