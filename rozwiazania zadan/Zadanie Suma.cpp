514625f9cdba1d165fa845650e2a3fe9789f7cb7
Kalina <102462480+AlgorytmicznaPrzygodaKaliny@users.noreply.github.com>
2022-07-14 12:58:46 +0200

    Create Zadanie Suma.cpp

---
+++ rozwiazania zadan/Zadanie Suma.cpp
@@ -0,0 +1,17 @@
+//Zadanie Suma
+//https://szkopul.edu.pl/problemset/problem/1wW3HqV_XcFb2FsCyCXNR9_i/site/?key=statement
+//Kod: Kalina Staszewska
+//Wynik: 100/100 punktów
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;	
+    
+    for(int i=0; i<k; i++) cout << 1;
+    for(int i=0; i<k-1; i++) cout << 0;
+    
+	return 0;
+}
